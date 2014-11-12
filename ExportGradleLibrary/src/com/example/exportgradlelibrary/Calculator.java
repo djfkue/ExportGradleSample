1f97ad0f7ea645a46c940db33fb51134d95174e3
Pengxiao Du <pengxiao.du@visionure.com>
2014-11-12 11:28:08 +0800

    add some code

---
+++ ExportGradleLibrary/src/com/example/exportgradlelibrary/Calculator.java
@@ -0,0 +1,10 @@
+package com.example.exportgradlelibrary;
+
+/**
+ * Created by argon on 14-11-12.
+ */
+public class Calculator {
+    public static int add(int a, int b) {
+        return a+b;
+    }
+}
